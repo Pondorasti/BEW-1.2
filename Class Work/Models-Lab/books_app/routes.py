43f33177f070356f1cc9db5f8fae15e19dea47df
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-08 11:27:35 -0700

    Add Lesson 3 Class Work

---
+++ Class Work/Models-Lab/books_app/routes.py
@@ -0,0 +1,26 @@
+"""Import packages and modules."""
+import os
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from datetime import date, datetime
+from books_app.models import Book, Author, Genre, User
+
+# Import app and db from events_app package so that we can run app
+from books_app import app, db
+
+main = Blueprint("main", __name__)
+
+##########################################
+#           Routes                       #
+##########################################
+
+
+@main.route('/')
+def homepage():
+    users = User.query.all()
+    return render_template('home.html', users=users)
+
+
+@main.route('/profile/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).one()
+    return render_template('profile.html', user=user)
