27cddd94c2d144dc523995c3ee385db3557ae4ec
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-04-29 17:28:52 -0700

    Final Project: Add auth flow

--- Class Work/final-project/final_app/auth/routes.py
+++ Class Work/final-project/final_app/auth/routes.py
@@ -1,13 +1,48 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required, current_user
-
-from final_app import app, db
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from .. import app, db, bcrypt
+from ..models import User
+from .forms import SignUpForm, LoginForm
 
 auth = Blueprint("auth", __name__)
 
 # Routes
 
 
-@auth.route("/signup")
-def homepage():
-    return "Soon TM"
+@auth.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode("utf-8")
+        user = User(username=form.username.data, password=hashed_password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash("New user created!")
+
+        return redirect(url_for("auth.login"))
+
+    return render_template("signup.html", form=form, action="/signup")
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get("next")
+        return redirect(next_page if next_page else url_for("main.homepage"))
+
+    return render_template("login.html", form=form, action="/login")
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.homepage"))
